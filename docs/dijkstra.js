ec76f4ab9aefcaed4fa82e2655459b0fb5a5f507
SujitNasahik <sujitnashik.nashik4@gmail.com>
2023-04-23 03:50:26 -0700

    added buttons for different algorithm

---
+++ docs/dijkstra.js
@@ -0,0 +1,47 @@
+function dijkstra(startId, goalId) {
+    let data = JSON.parse(localStorage.getItem('data'));
+
+    let dist = {};
+    let previous = {};
+    let unvisited = new Set(Object.keys(data));
+
+    for (let key in data) {
+        dist[key] = Infinity;
+        previous[key] = null;
+    }
+    dist[startId] = 0;
+
+    while (unvisited.size > 0) {
+        let currentId = getClosestNode(dist, unvisited);
+        unvisited.delete(currentId);
+
+        if (currentId == goalId) {
+            return {previous: previous};
+        }
+
+        for (let neighbor of data[currentId].con) {
+            let alt = dist[currentId] + distance(data[currentId].lat, data[currentId].lon, data[neighbor].lat, data[neighbor].lon);
+
+            if (alt < dist[neighbor]) {
+                dist[neighbor] = alt;
+                
+				                previous[neighbor] = currentId;
+            }
+        }
+    }
+
+    return null;
+}
+function getClosestNode(dist, unvisited) {
+    let minDist = Infinity;
+    let closestNode = null;
+
+    for (let node of unvisited) {
+        if (dist[node] < minDist) {
+            minDist = dist[node];
+            closestNode = node;
+        }
+    }
+
+    return closestNode;
+}
